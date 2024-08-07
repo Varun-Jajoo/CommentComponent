5919852dfbbafb6c2897b473ac5865b7552f820a
Shreyans jain <shreyansjain1603@gmail.com>
2024-08-07 21:53:12 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import Component from "./Component";
 function App() {
   return (
     <>
-      <Component positionLeft={135} bool={false} correction={66}/>
-      <Component positionLeft={90} bool={false} correction={44}/>
-      <Component positionLeft={45} bool={false} correction={22}/>
-      <Component positionLeft={0} bool={true} correction={0}/>
+      <Component positionLeft={135} first={true} last={false} correction={66} />
+      <Component positionLeft={90} first={false} last={false} correction={44} />
+      <Component positionLeft={45} first={false} last={false} correction={22} />
+      <Component positionLeft={0} first={false} last={true} correction={0} />
     </>
   );
 }
--- src/Component.tsx
+++ src/Component.tsx
@@ -1,38 +1,47 @@
-import React from 'react';
+import React from "react";
 import { IoMdDownload } from "react-icons/io";
-import connector from './connector.svg';
+import connector from "./connector2.svg";
 
 interface ComponentProps {
-    positionLeft: number;
-    bool:boolean;
-    correction:number;
+  positionLeft: number;
+  last: boolean;
+  correction: number;
+  first: boolean;
 }
 
-const Component: React.FC<ComponentProps> = ({ positionLeft,bool,correction }) => {
+const Component: React.FC<ComponentProps> = ({
+  positionLeft,
+  last,
+  correction,
+  first
+}) => {
   const dynamicWidth = 350 - positionLeft;
 
   return (
-    <div className='flex justify-center w-screen mb-8 relative'>
-        <div
-            style={{
-                position: "relative",
-                left: `${bool ? positionLeft : positionLeft-correction }px`,
-                width: `${dynamicWidth}px`
-            }}
-            className={`flex gap-14 items-center bg-gray-200 rounded-md px-4 py-3 z-10 justify-between`}
-        >
-            <img
-                src={connector}
-                className="absolute top-[-71.5px] left-0 w-10 z-[-1]"
-                alt="logo"
-            />
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
-                Default
-            </button>
-            <div>
-                <IoMdDownload />
-            </div>
+    <div className="flex justify-center w-screen mb-8 relative">
+      <div
+        style={{
+          position: "relative",
+          left: `${last ? positionLeft : positionLeft - correction}px`,
+          width: `${dynamicWidth}px`,
+        }}
+        className={`flex gap-14 items-center bg-gray-200 rounded-md px-4 py-3 z-10 justify-between`}
+      >
+        {!first && (
+          <img
+            src={connector}
+            className="absolute top-[-58.5px] left-2 w-10 z-[-1]"
+            alt="logo"
+          />
+        )}
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Default
+        </button>
+        <div>
+          <IoMdDownload />
         </div>
+      </div>
     </div>
   );
 };
