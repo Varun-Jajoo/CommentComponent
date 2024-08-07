12920d673979672255811e8de4b81ecca55e45af
varun jajoo <varun.jajoo18@gmail.com>
2024-08-07 19:29:21 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { IoMdDownload } from "react-icons/io";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex justify-center items-center h-screen'>
+      <div className='flex gap-14 items-center bg-gray-200 rounded-md px-4 py-3'>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+          default
+        </button>
+        <div>
+        <IoMdDownload />
+        </div>
+      </div>
     </div>
+   
   );
 }
 
